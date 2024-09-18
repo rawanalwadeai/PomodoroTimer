d95027e0c605c6c3d52fd57cc61cf1c5bd16863b
rawanalwadeai <alwadeairawan@gmail.com>
2024-09-18 23:58:34 +0300

    Updates

---
+++ precache-manifest.90f9015fe62c5a0463833ec08b5e0c89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc54163be451d6407f95281c22eb6f9e",
+    "url": "/PomodoroTimer/index.html"
+  },
+  {
+    "revision": "0c60b930f3777af91bd7",
+    "url": "/PomodoroTimer/static/css/2.1f505516.chunk.css"
+  },
+  {
+    "revision": "29cd1176bc021f300de6",
+    "url": "/PomodoroTimer/static/css/main.1bfdb217.chunk.css"
+  },
+  {
+    "revision": "0c60b930f3777af91bd7",
+    "url": "/PomodoroTimer/static/js/2.c6f2e33e.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/PomodoroTimer/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "29cd1176bc021f300de6",
+    "url": "/PomodoroTimer/static/js/main.a9a297bf.chunk.js"
+  },
+  {
+    "revision": "c8ae6f6c885e0210443a",
+    "url": "/PomodoroTimer/static/js/runtime~main.2c777275.js"
+  }
+]);
